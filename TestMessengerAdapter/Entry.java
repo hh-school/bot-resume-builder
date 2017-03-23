2517e5a2caa13baa57575769986caccee53414db
Trympyrym <rio09876@gmail.com>
2017-03-23 08:26:15 +0300

    TestMessengerAdapter added (#4)
    * TestMessengerAdapter added

---
+++ TestMessengerAdapter/Entry.java
@@ -0,0 +1,44 @@
+import java.util.regex.Pattern;
+
+public class Entry
+{
+    private Pattern pattern;
+    private String answer;
+    private int weight;
+
+    public Entry() {
+    }
+
+    public Pattern getPattern() {
+        return pattern;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setPattern(Pattern pattern) {
+        this.pattern = pattern;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Entry{" +
+                "pattern=" + pattern +
+                ", answer='" + answer + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+}
---
+++ TestMessengerAdapter/Loader.java
@@ -0,0 +1,66 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class Loader {
+
+    private final String questionMark = "question=";
+    private final String answerMark = "answer=";
+    private final String weightMark = "weight=";
+
+    private List<Entry> entries;
+    private Entry currentEntry;
+    private StringBuilder stringBuilder;
+
+    public Loader(List<Entry> entries) {
+        this.entries = entries;
+    }
+
+    public void load(String filename) throws IOException {
+
+        Files.lines(Paths.get(filename)).forEach((String line) -> {loadLine(line);});
+    }
+
+        private void loadLine(String line)
+    {
+        if (questionBegins(line))
+        {
+            currentEntry = new Entry();
+            stringBuilder = new StringBuilder(line.substring(questionMark.length()));
+            return;
+        }
+
+        if (answerBegins(line))
+        {
+            currentEntry.setPattern(Pattern.compile(stringBuilder.toString()));
+            stringBuilder = new StringBuilder(line.substring(answerMark.length()));
+            return;
+        }
+
+        if (weightBegins(line))
+        {
+            currentEntry.setAnswer(stringBuilder.toString());
+            currentEntry.setWeight(Integer.parseInt(line.substring(weightMark.length())));
+            entries.add(currentEntry);
+            return;
+        }
+
+        stringBuilder.append(System.lineSeparator());
+        stringBuilder.append(line);
+    }
+
+    private boolean questionBegins(String line)
+    {
+        return line.startsWith(questionMark);
+    }
+    private boolean weightBegins(String line)
+    {
+        return line.startsWith(weightMark);
+    }
+    private boolean answerBegins(String line)
+    {
+        return line.startsWith(answerMark);
+    }
+}
