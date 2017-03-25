822d018bfb6bc47a070faf65b04e54b279612566
Gregory Stepanov <rio09876@gmail.com>
2017-03-25 15:13:09 +0300

    getIndex added to ChatId

--- src/main/java/ru/hh/resumebuilderbot/ChatId.java
+++ src/main/java/ru/hh/resumebuilderbot/ChatId.java
@@ -1,6 +1,10 @@
 package ru.hh.resumebuilderbot;
 
 public class ChatId {
+    public long getIndex() {
+        return index;
+    }
+
     // todo: make private and override constructor
     final long index;
 
