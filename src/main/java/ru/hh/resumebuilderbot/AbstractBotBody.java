579476ad135e541adbb11ee3c863c6318c4a19be
Sergey Savin <savin.sergey91@gmail.com>
2017-04-10 14:40:58 +0300

    BotBody interface rename

--- src/main/java/ru/hh/resumebuilderbot/AbstractBotBody.java
+++
@@ -1,7 +0,0 @@
-package ru.hh.resumebuilderbot;
-
-public interface AbstractBotBody {
-    void answer(Answer answer, int timeoutMs);
-
-    void setMessenger(MessengerAdapter messenger);
-}
--- src/main/java/ru/hh/resumebuilderbot/BotBody.java
+++ src/main/java/ru/hh/resumebuilderbot/BotBody.java
@@ -1,24 +1,7 @@
 package ru.hh.resumebuilderbot;
 
-import ru.hh.resumebuilderbot.message.handler.MessageHandler;
-import ru.hh.resumebuilderbot.question.generator.QuestionGeneratorsQueue;
+public interface BotBody {
+    void answer(Answer answer, int timeoutMs);
 
-import java.util.Queue;
-
-public class BotBody implements AbstractBotBody {
-    private MessengerAdapter messengerAdapter;
-
-    @Override
-    public void answer(Answer answer, int timeoutMs) {
-        MessageHandler messageHandler = Selector.select(answer);
-        QuestionGeneratorsQueue questionGeneratorQueue = messageHandler.handle(answer);
-        ChatId chatId = answer.getChatId();
-        Queue<Question> questions = questionGeneratorQueue.generateQuestions(chatId);
-        questions.forEach(messengerAdapter::ask);
-    }
-
-    @Override
-    public void setMessenger(MessengerAdapter messenger) {
-        this.messengerAdapter = messenger;
-    }
+    void setMessenger(MessengerAdapter messenger);
 }
