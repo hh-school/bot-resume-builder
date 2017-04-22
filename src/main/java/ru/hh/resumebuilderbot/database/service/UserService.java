7fa709dcee3ff4cc18b9be157cc3e10800b63545
Bulat Khasanov <afti@yandex.ru>
2017-04-22 14:12:34 +0300

    fix user model import, remove unused SalaryCurrency service, dao

--- src/main/java/ru/hh/resumebuilderbot/database/service/UserService.java
+++ src/main/java/ru/hh/resumebuilderbot/database/service/UserService.java
@@ -1,6 +1,6 @@
 package ru.hh.resumebuilderbot.database.service;
 
-import ru.hh.resumebuilderbot.User;
+import ru.hh.resumebuilderbot.database.model.User;
 import ru.hh.resumebuilderbot.database.service.base.GenericService;
 
 public interface UserService extends GenericService<User, Integer> {
--- src/main/java/ru/hh/resumebuilderbot/database/service/UserServiceImpl.java
+++ src/main/java/ru/hh/resumebuilderbot/database/service/UserServiceImpl.java
@@ -1,8 +1,8 @@
 package ru.hh.resumebuilderbot.database.service;
 
 import org.hibernate.SessionFactory;
-import ru.hh.resumebuilderbot.User;
 import ru.hh.resumebuilderbot.database.dao.UserDAO;
+import ru.hh.resumebuilderbot.database.model.User;
 import ru.hh.resumebuilderbot.database.service.base.GenericServiceImpl;
 
 import javax.inject.Inject;
