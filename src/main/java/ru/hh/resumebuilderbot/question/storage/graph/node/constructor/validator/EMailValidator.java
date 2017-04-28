0931b6c290374d8e5c381d13940de67eb844ffac
Gregory Stepanov <rio09876@gmail.com>
2017-04-28 09:02:01 +0300

    patternValidator

--- src/main/java/ru/hh/resumebuilderbot/question/storage/graph/node/constructor/validator/EMailValidator.java
+++ src/main/java/ru/hh/resumebuilderbot/question/storage/graph/node/constructor/validator/EMailValidator.java
@@ -1,14 +1,11 @@
 package ru.hh.resumebuilderbot.question.storage.graph.node.constructor.validator;
 
-import ru.hh.resumebuilderbot.Answer;
-
 import java.util.regex.Pattern;
 
-public class EMailValidator extends Validator {
-    @Override
-    public boolean answerIsValid(Answer answer) {
-        String email = (String) answer.getAnswerBody();
-        return Pattern.compile(".+@.+").matcher(email).matches();
+public class EMailValidator extends PatternValidator {
+
+    public EMailValidator() {
+        pattern = Pattern.compile(".+@.+");
     }
 
     @Override
